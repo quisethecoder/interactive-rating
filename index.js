6b99e870f92fa23af11bfbb91ba8d917b7cb381a
Marquise <marquise_dawkins@aol.com>
2024-03-25 22:42:38 -0400

    delete code

--- index.js
+++ index.js
@@ -1,11 +1,2 @@
 
-const change = document.querySelector(".num");
-
-
-function stick(event){
-   
-    event.style.backgroundColor = "rgb(165, 82, 82)";
-    event.style.color = "rgb(89, 89, 178)";
-}
-
 
