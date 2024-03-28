549a6f42ed5d037a55a2129bf87bfcbee59c33c3
Marquise <marquise_dawkins@aol.com>
2024-03-27 23:52:11 -0400

    new div styling

--- index.js
+++ index.js
@@ -25,17 +25,22 @@ document.addEventListener("DOMContentLoaded", function () {
             const message = "You've selected " + selectedNumber + " out of 5.";
             const headerMessage = "Thank you!";
             const additionalMessage = "We want to thank you for taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!";
+            
             const ratingMessage = document.createElement("p");
             ratingMessage.textContent = message;
+            ratingMessage.classList.add("rating"); 
 
             const penIcon = document.createElement("i");
             penIcon.classList.add("fa-solid", "fa-pen-to-square");
 
             const headMess = document.createElement("h1");
             headMess.textContent = headerMessage;
+            headMess.classList.add("headMess");
 
             const additionalMessageElem = document.createElement("p");
             additionalMessageElem.textContent = additionalMessage;
+            additionalMessageElem.classList.add("additional-message");
+
             wrapper.innerHTML = ""; // Clear previous content
             wrapper.appendChild(penIcon);
             wrapper.appendChild(ratingMessage);
