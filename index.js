f80e2cf2797d6aeae47d454c35cab9883b152162
Marquise <marquise_dawkins@aol.com>
2024-03-27 23:20:15 -0400

    Message Pop Up

--- index.js
+++ index.js
@@ -1,2 +1,48 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const nums = document.querySelectorAll(".num");
+    const submitBtn = document.querySelector(".btn");
+    const wrapper = document.querySelector(".wrapper");
+    const container = document.querySelector(".container");
 
+    // Variable to store the selected number
+    let selectedNumber = 0;
 
+    // Function to handle click event on numbers
+    function handleNumberClick(event) {
+        selectedNumber = parseInt(event.target.textContent);
+        console.log("You selected:", selectedNumber);
+    }
+
+    // Add click event listener to each number
+    nums.forEach(num => {
+        num.addEventListener("click", handleNumberClick);
+    });
+
+    // click event listener submit button
+    submitBtn.addEventListener("click", function () {
+        //message with the selected number
+        if (selectedNumber > 0) {
+            const message = "You've selected " + selectedNumber + " out of 5.";
+            const headerMessage = "Thank you!";
+            const additionalMessage = "We want to thank you for taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!";
+            const ratingMessage = document.createElement("p");
+            ratingMessage.textContent = message;
+
+            const penIcon = document.createElement("i");
+            penIcon.classList.add("fa-solid", "fa-pen-to-square");
+
+            const headMess = document.createElement("h1");
+            headMess.textContent = headerMessage;
+
+            const additionalMessageElem = document.createElement("p");
+            additionalMessageElem.textContent = additionalMessage;
+            wrapper.innerHTML = ""; // Clear previous content
+            wrapper.appendChild(penIcon);
+            wrapper.appendChild(ratingMessage);
+            wrapper.appendChild(headMess);
+            wrapper.appendChild(additionalMessageElem);
+        } else {
+            console.log("Please select a rating before submitting. There was an error");
+        }
+    });
+});
